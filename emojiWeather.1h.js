807289f55a855c267a90ca7e75f262e45d03d6c2
Chandler Davidson <chandlermdavidson@gmail.com>
2017-10-11 20:24:02 -0500

    Cleaned Up

--- emojiWeather.1h.js
+++ emojiWeather.1h.js
@@ -2,51 +2,31 @@
 
 let request = require('request');
 
-let apiKey = '49f7841c12274550a57cc63dd69172c1';
+let apiKey = '';
 let city = 'Huntsville';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-request(url, function (err, response, body) {
-  if(err){
-    console.log('err:', error);
-  } else {
-    let weather = JSON.parse(body)
-    //let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-    let message = weather.weather[0].id;
-    console.log(getEmoji(Math.ceil(message/100)*100));  // Round to weather ID group
-  }
+let emojis = [
+    ':cry:',        // No connection
+    '::',           // 
+    ':zap:',        // Thunderstorm
+    ':drizzle:',    // Rain
+    '::',           // 
+    ':cloud:',      // Cloudy
+    ':snowflake:',  // Snow
+    '::',           // 
+    ':sunny:',      // Clear
+    ':tornado:']    // Extreme
+
+request(url, function (err, response, body) {   // Collect JSON object
+    if (err) {  // If no connection, so sad...
+        console.log(emojis[0]);
+    } else {
+
+        let weather = JSON.parse(body)
+        let id = weather.weather[0].id; // Seperate the weather ID
+        id = Math.ceil(id / 100);       // Get only the hundreds place (1xx, 2xx)
+
+        console.log(emojis[id]);        // Print the emoji 
+    }
 });
-
-function getEmoji(conditions) {
-  switch (conditions) {
-    case 200:
-    // Thunderstorm
-    return ":zap:";
-    break;
-
-    case 300:
-    // Drizzle
-    return "Drizzle";
-    break;
-
-    case 500:
-    // Rain
-    return ":cloud:";
-    break;
-
-    case 600:
-    // Snow
-    return ":snowflake:";
-    break;
-
-    case 800:
-    // Clear
-    return ":sunny:";
-    break;
-
-    case 900:
-    // Extreme
-    return ":tornado:";
-    break;
-  }
-}
